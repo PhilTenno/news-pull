1ac9d8ee403d423034134ee2644ee23a220a6800
Phil <git@hardbitrocker.de>
2025-05-13 12:32:42 +0200

    Änderungen tl_settings/language/tl_settings

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -1,11 +1,8 @@
 <?php
 
-error_log('DCA tl_settings.php wird geladen: ' . date('c'));
-file_put_contents(__DIR__ . '/../../../../news_pull_debug.txt', 'tl_settings.php geladen: ' . date('c') . "\n", FILE_APPEND);
-
 $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace(
     '{chmod_legend}',
-    '{news_pull_legend},news_pull_upload_dir,news_pull_image_dir,news_pull_auto_publish;{chmod_legend}',
+    '{news_pull_legend},news_pull_upload_dir,news_pull_image_dir,news_pull_news_archive,news_pull_auto_publish;{chmod_legend}',
     $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']
 );
 
@@ -21,6 +18,14 @@ $GLOBALS['TL_DCA']['tl_settings']['fields']['news_pull_image_dir'] = [
     'inputType' => 'fileTree',
     'eval'      => ['fieldType' => 'radio', 'filesOnly' => false, 'mandatory' => true, 'tl_class' => 'w50'],
     'sql'       => "varchar(255) NOT NULL default ''"
+];
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['news_pull_news_archive'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_settings']['news_pull_news_archive'],
+    'inputType' => 'select',
+    'foreignKey'=> 'tl_news_archive.title',
+    'eval'      => ['mandatory'=>true, 'chosen'=>true, 'tl_class'=>'w50'],
+    'sql'       => "int(10) unsigned NOT NULL default '0'"
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['news_pull_auto_publish'] = [
--- src/Resources/contao/languages/de/tl_settings.php
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -4,3 +4,4 @@ $GLOBALS['TL_LANG']['tl_settings']['news_pull_legend'] = 'News-Pull Einstellunge
 $GLOBALS['TL_LANG']['tl_settings']['news_pull_upload_dir'] = ['Upload-Verzeichnis', 'Verzeichnis für neue News (JSON & Bilder)'];
 $GLOBALS['TL_LANG']['tl_settings']['news_pull_image_dir'] = ['Bilder-Verzeichnis', 'Zielverzeichnis für importierte Bilder'];
 $GLOBALS['TL_LANG']['tl_settings']['news_pull_auto_publish'] = ['News automatisch veröffentlichen', 'News nach Import automatisch veröffentlichen'];
+$GLOBALS['TL_LANG']['tl_settings']['news_pull_news_archive'] = ['Nachrichtenarchiv', 'Wählen Sie das Nachrichtenarchiv, in das die News importiert werden sollen.'];
