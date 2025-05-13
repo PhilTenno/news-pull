31c67df569410d38a015ef4a27bb89805b0ac510
Phil <git@hardbitrocker.de>
2025-05-13 13:57:35 +0200

    PublicImportController & services.yaml

--- src/Controller/PublicImportController.php
+++ src/Controller/PublicImportController.php
@@ -2,14 +2,24 @@
 
 namespace PhilTenno\NewsPull\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use PhilTenno\NewsPull\Service\NewsImportService;
 
-class PublicImportController
+class PublicImportController extends AbstractController
 {
+    private NewsImportService $newsImportService;
+
+    public function __construct(NewsImportService $newsImportService)
+    {
+        $this->newsImportService = $newsImportService;
+    }
+
     #[Route('/news-pull/import', name: 'news_pull_import', methods: ['GET'])]
     public function __invoke(): Response
     {
+        $this->newsImportService->importNews();
         return new Response('News-Pull Import ausgeführt!');
     }
 }
