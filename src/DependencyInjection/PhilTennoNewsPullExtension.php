ba8563fbf9826c99601980292a7aa9c7508401a9
Phil <git@hardbitrocker.de>
2025-05-15 17:32:43 +0200

    services

---
+++ src/DependencyInjection/PhilTennoNewsPullExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PhilTenno\NewsPull\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class PhilTennoNewsPullExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new \Symfony\Component\DependencyInjection\Loader\YamlFileLoader(
+            $container,
+            new \Symfony\Component\Config\FileLocator(__DIR__ . '/../Resources/config')
+        );
+        $loader->load('services.yaml');
+    }
+}
