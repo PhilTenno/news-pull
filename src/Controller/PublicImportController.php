e20f5c01b0cb8acf3e338977a8bbaa85dfb496ad
Phil <git@hardbitrocker.de>
2025-05-16 18:50:52 +0200

    fixes

--- src/Controller/PublicImportController.php
+++ src/Controller/PublicImportController.php
@@ -2,24 +2,26 @@
 
 namespace PhilTenno\NewsPull\Controller;
 
+use PhilTenno\NewsPull\Service\NewsImportService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use PhilTenno\NewsPull\Service\NewsImportService;
 
 class PublicImportController extends AbstractController
 {
-    private NewsImportService $newsImportService;
+    public function __construct(
+        private NewsImportService $newsImportService,
+        private LoggerInterface $logger
+    ) {}
 
-    public function __construct(NewsImportService $newsImportService)
+    public function importAction(): Response
     {
-        $this->newsImportService = $newsImportService;
-    }
-
-    #[Route('/news-pull/import', name: 'news_pull_import', methods: ['GET'])]
-    public function __invoke(): Response
-    {
-        $this->newsImportService->importNews();
-        return new Response('News-Pull Import ausgeführt!');
+        try {
+            $this->newsImportService->importNews();
+            return new Response('News erfolgreich importiert', Response::HTTP_OK);
+        } catch (\Exception $e) {
+            $this->logger->error('Import fehlgeschlagen: ' . $e->getMessage());
+            return new Response('Fehler beim Import', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
