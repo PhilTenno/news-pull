8a0e44224189a6bbb159b929ba16f58fc6ce7a36
Phil <git@hardbitrocker.de>
2025-05-20 14:52:21 +0200

    add config

---
+++ contao/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// contao/config/config.php
+
+$GLOBALS['BE_MOD']['system']['news_pull'] = [
+    'tables' => ['tl_newspull'],
+    // Optional: eigenes Icon, falls vorhanden
+    // 'icon'   => 'bundles/philtennonewspull/icon.svg',
+];
