555755cf610c97e899263ac6f74223dee073715d
Phil <git@hardbitrocker.de>
2025-05-22 13:39:22 +0200

    fixes Plugin

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,5 +1,5 @@
 <?php
-//src/ContaoManager/Plugin.php
+// src/ContaoManager/Plugin.php
 declare(strict_types=1);
 
 namespace PhilTenno\NewsPull\ContaoManager;
@@ -8,8 +8,11 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 
-class Plugin implements BundlePluginInterface
+class Plugin implements BundlePluginInterface, RoutingPluginInterface
 {
     public function getBundles(ParserInterface $parser)
     {
@@ -17,5 +20,12 @@ class Plugin implements BundlePluginInterface
             BundleConfig::create(\PhilTenno\NewsPull\PhilTennoNewsPullBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
+    }
+
+    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
+    {
+        return $resolver
+            ->resolve(__DIR__ . '/../../config/routes.yaml')
+            ->load(__DIR__ . '/../../config/routes.yaml');
     }
 }
